7fd9d297bb0f3280ef550bf3cfdf983fd34004fb
nicola00 <nicolajobs00@gmail.com>
2023-02-23 11:42:33 +0100

    FEAT add font-size and array

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const { createApp } = Vue
 
-createApp({
-    data(){
-        return{
-            
-
-        }
+const contacts= [
+    {
+        name: 'Michele',
+        avatar: './img/avatar_1.png',
+        visible: true,
+        messages: [
+            {
+                date: '10/01/2020 15:30:55',
+                message: 'Hai portato a spasso il cane?',
+                status: 'sent'
+            },
+            {
+                date: '10/01/2020 15:50:00',
+                message: 'Ricordati di stendere i panni',
+                status: 'sent'
+            },
+            {
+                date: '10/01/2020 16:15:22',
+                message: 'Tutto fatto!',
+                status: 'received'
+            },
+        ],
     },
+]
+    
+createApp({
+    data() {
+        return {
+        message: 'Hello Vue!'
+        }
+    }
 }).mount('#app')
